266cddefc823c5077f94e873fe93655df7508035
Patrick O Connor <padraigoc@gmail.com>
2019-03-07 01:13:42 +0000

    updating exercise files

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+// function sum(fromN, toN) {
+  
+//   if(toN === fromN) {
+//   return toN;
+//   } 
+//   return fromN + sum(fromN-1, toN);
+  
+// }
+
+//  console.log(sum(7,3));
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  
+  if(toN === fromN) {
+  return toN;
+  } 
+  return fromN + sum(fromN+1, toN);
+  
 }
 
+ console.log(sum(3,7));
+
 module.exports = sum;
+
