76b48e0bf500d36a8ce6c3923dfe61ebd932614d
Patrick O Connor <padraigoc@gmail.com>
2019-03-07 01:14:59 +0000

    refactored code

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-// function sum(fromN, toN) {
-  
-//   if(toN === fromN) {
-//   return toN;
-//   } 
-//   return fromN + sum(fromN-1, toN);
-  
-// }
-
-//  console.log(sum(7,3));
 
 function sum(fromN, toN) {
-  
   if(toN === fromN) {
-  return toN;
+    return toN;
   } 
   return fromN + sum(fromN+1, toN);
-  
 }
-
- console.log(sum(3,7));
+console.log(sum(3,7));
 
 module.exports = sum;
 
